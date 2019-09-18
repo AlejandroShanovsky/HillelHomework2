61b5ea736afbb6bbab3237be964a08304ee680a2
Alexander Shanovsky <shanovskiy2016@gmail.com>
2019-09-18 15:35:53 +0300

    Commit

--- springAnnotationBased/src/main/java/com/project/springAnnotationBased/SpringAnnotationBasedStarter.java
+++ springAnnotationBased/src/main/java/com/project/springAnnotationBased/SpringAnnotationBasedStarter.java
@@ -4,12 +4,10 @@ import com.project.springAnnotationBased.config.SpringAnnotationBasedConfig;
 import com.project.springAnnotationBased.entitys.student.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class SpringAnnotationBasedStarter {
     public static void main(String[] args) {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringAnnotationBasedConfig.class);
-//        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:app-context.xml");
         Student student = ctx.getBean("student", Student.class);
         System.out.println(student.study());
     }
